e29ffa328912a8498970fc8e3f3d57e9a48eda62
Parv Gupta <Parvg555@gmail.com>
2023-02-21 15:01:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 function binarySearch(nums, target) {
-// Your code here
-// Return index if target is present
-// else return -1
+  let start = 0,
+    end = nums.length - 1;
+  let index = -1;
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+
+    if (nums[mid] === target) {
+      index = mid;
+      return index;
+    } else if (nums[mid] < target) start = mid + 1;
+    else end = mid - 1;
+  }
+  return index;
 }
 
 module.exports = binarySearch;
